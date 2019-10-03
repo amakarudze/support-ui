eeb78c1061227cda3c1086279e12e1d731f4bbcb
Anna <anna.makarudze@britecore.com>
2019-10-03 21:01:56 +0200

    Connect frontend to FAQs API

--- src/components/Help.vue
+++ src/components/Help.vue
@@ -8,12 +8,44 @@
       <p>If you cannot find a solution for your problem here, please feel free to chat with our support team via Live Chat.</p>
       <router-link class="btn btn-primary btn-lg" to="/livechat" role="button">Live Chat</router-link>
     </div>
+    <div class="row">
+       <div class="col-4">
+         <h2>Browse by Categories</h2>
+         <ul>
+           <li v-for="category in categories" :key="category.id">
+             {{ category }}
+           </li>
+         </ul>
+       </div>
+       <div class="col-8">
+         <h2>Frequently Asked Questions</h2>
+        </div>
+    </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
-  name: 'help'
+  name: 'help',
+  data () {
+    return {
+      categories: null,
+      errored: false
+    }
+  },
+  mounted () {
+    axios
+      .get('http://127.0.0.1:8000/faqs/categories/')
+      .then(response => {
+        this.categories = response.queryset
+        console.log(this.categories)
+      })
+      .catch(error => {
+        console.log(error)
+        this.errored = true
+      })
+  }
 }
 </script>
 
